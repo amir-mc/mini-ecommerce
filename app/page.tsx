3c398fbffc1ebb2776020694d4a1b85072636173
amir-mc <amirfooladi93@gmail.com>
2025-04-22 18:56:33 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Hero from "@/section/hero";
+import {  TreeDeciduous } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
   <div className="flex flex-col items-center">
-    <div>
-      E-commerce 
-    </div>
+    <Hero/>
   </div>
   );
 }
---
+++ section/hero.tsx
@@ -0,0 +1,11 @@
+import { CarouselDemo } from "@/components/product/product";
+
+const Hero = () => {
+    return ( 
+        <div className="">
+            <CarouselDemo/>
+        </div>
+     );
+}
+ 
+export default Hero;
