7cc92889b5792ec98ca2425cc83f051aa40b7eed
amir-mc <amirfooladi93@gmail.com>
2025-04-22 16:22:49 +0000

    add husky

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
   <div className="flex flex-col items-center">
-    First Commit
+    <div>
+      E-commerce 
+    </div>
   </div>
   );
 }
